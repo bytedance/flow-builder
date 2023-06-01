4ddbb2e81d9191a103744410f18eee6267e22920
lixiao.1022 <564113123@qq.com>
2023-06-01 15:14:03 +0800

    Chore deploy site (#47)
    * fix: ico
    
    * fix: remove 备案号

--- .dumi/theme/layout.tsx
+++ .dumi/theme/layout.tsx
@@ -1,18 +1,9 @@
 import React from 'react';
 import Layout from 'dumi-theme-default/es/layout';
 
-import './layout.css';
-
 const CustomLayout = (props) => (
   <Layout {...props}>
-    <>
-      {props.children}
-      <div className="record-number">
-        <a href="https://beian.miit.gov.cn/" target="_blank">
-          浙ICP备2022016932号-1
-        </a>
-      </div>
-    </>
+    <>{props.children}</>
   </Layout>
 );
 
